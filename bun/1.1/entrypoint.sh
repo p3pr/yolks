0097756d76aed4d655974e2f87f1649d70da497a
benjamint08 <benjamint08@gmail.com>
2024-04-02 15:43:40 +0100

    Hi

--- bun/1.1/entrypoint.sh
+++ bun/1.1/entrypoint.sh
@@ -1,1 +1,6 @@
-cd /home/container && bun install && bun run .
+cd /home/container
+ls
+bun --version
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+eval ${MODIFIED_STARTUP}
