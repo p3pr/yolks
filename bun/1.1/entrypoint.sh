bb7804689a4f9898d6adb259a27e2f0113128ba2
ben <61296321+benjamint08@users.noreply.github.com>
2024-04-13 20:08:56 +0100

    Update entrypoint.sh

--- bun/1.1/entrypoint.sh
+++ bun/1.1/entrypoint.sh
@@ -6,6 +6,8 @@ cd /home/container
 # Check if the 'bun' command is available
 if command -v bun &> /dev/null; then
     bun --version
+    echo "Trying to upgrade Bun..."
+    bun upgrade
 else
     echo "bun is not installed or not found in PATH"
 fi
